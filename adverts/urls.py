14a944c3ab6410585b908b5f4e02568b661fff70
kad-ir <kadieyr@gmail.com>
2023-04-15 11:30:16 +0300

    rendering vacancies

--- adverts/urls.py
+++ adverts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('adverts/', views.adverts_page, name="adverts")
+    path('adverts/', views.adverts_page, name="adverts"),
+    path('advert/<str:pk>/', views.advert_page, name="advert"),
 ]
--- adverts/views.py
+++ adverts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from . models import Advert
 # Create your views here.
 
 
 def adverts_page(request):
-    context = {}
+    adverts = Advert.objects.all
+    context = {'adverts': adverts}
     return render(request, 'adverts/adverts.html', context)
+
+
+def advert_page(request, pk):
+    advert = Advert.objects.get(id=pk)
+    context = {'advert': advert}
+    return render(request, 'adverts/advert.html', context)
